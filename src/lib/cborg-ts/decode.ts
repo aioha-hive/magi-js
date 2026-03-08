8233e7bfdea5adec7409f4efb4fb790e8ce4741a
techcoderx <git@techcoderx.com>
2026-03-08 23:28:59 +0800

    evm: cleanup redundant cborg decoder

--- src/lib/cborg-ts/decode.ts
+++ src/lib/cborg-ts/decode.ts
@@ -1,7 +1,29 @@
-import { decodeErrPrefix } from './common.js'
-import { Type, type Token } from './token.js'
+import { Type, Token } from './token.js'
+import type { TagDecoder } from './interface.js'
 import { jump, quick } from './jump.js'
-import type { DecodeOptions, DecodeTokenizer } from './interface.js'
+import { decodeErrPrefix } from './common.js'
+
+export interface DecodeTokenizer {
+  done(): boolean
+  next(): Token
+  pos(): number
+}
+
+export interface DecodeOptions {
+  allowIndefinite?: boolean
+  allowUndefined?: boolean
+  coerceUndefinedToNull?: boolean
+  allowInfinity?: boolean
+  allowNaN?: boolean
+  allowBigInt?: boolean
+  strict?: boolean
+  useMaps?: boolean
+  rejectDuplicateMapKeys?: boolean
+  retainStringBytes?: boolean
+  tags?: TagDecoder[]
+  tokenizer?: DecodeTokenizer
+  visitor?: (path: string[], value: unknown) => void
+}
 
 const defaultDecodeOptions = {
   strict: false,
@@ -10,18 +32,11 @@ const defaultDecodeOptions = {
   allowBigInt: true
 }
 
-/**
- * @implements {DecodeTokenizer}
- */
-class Tokeniser {
+class Tokeniser implements DecodeTokenizer {
   _pos: number
   data: Uint8Array
   options: DecodeOptions
 
-  /**
-   * @param {Uint8Array} data
-   * @param {DecodeOptions} options
-   */
   constructor(data: Uint8Array, options: DecodeOptions = {}) {
     this._pos = 0
     this.data = data
@@ -57,17 +72,18 @@ class Tokeniser {
 
 const DONE = Symbol.for('DONE')
 const BREAK = Symbol.for('BREAK')
+export const EMPTY_ARRAY = Symbol.for('EMPTY_ARRAY')
 
-/**
- * @param {Token} token
- * @param {DecodeTokenizer} tokeniser
- * @param {DecodeOptions} options
- * @returns {any|BREAK|DONE}
- */
-function tokenToArray(token: Token, tokeniser: DecodeTokenizer, options: DecodeOptions): any | typeof BREAK | typeof DONE {
-  const arr = []
+function tokenToArray(
+  token: Token,
+  tokeniser: DecodeTokenizer,
+  options: DecodeOptions,
+  path: string[],
+  ignoreToken: boolean = false
+): unknown[] | Record<string, unknown> {
+  const arr: unknown[] = []
   for (let i = 0; i < token.value; i++) {
-    const value = tokensToObject(tokeniser, options)
+    const value = tokensToObject(tokeniser, options, [...path, `_${i}_`], ignoreToken)
     if (value === BREAK) {
       if (token.value === Infinity) {
         // normal end to indefinite length array
@@ -80,27 +96,38 @@ function tokenToArray(token: Token, tokeniser: DecodeTokenizer, options: DecodeO
     }
     arr[i] = value
   }
+  if (arr.length === 0 && !ignoreToken) {
+    options.visitor?.(path, EMPTY_ARRAY)
+  }
+  const isObjArray = arr.some((val) => typeof val === 'object')
+  if (isObjArray) {
+    const res: Record<string, unknown> = {}
+    for (let i = 0; i < arr.length; i++) {
+      res[`_${i}_`] = arr[i]
+    }
+    return res
+  }
   return arr
 }
 
-/**
- * @param {Token} token
- * @param {DecodeTokenizer} tokeniser
- * @param {DecodeOptions} options
- * @returns {any|BREAK|DONE}
- */
-function tokenToMap(token: Token, tokeniser: DecodeTokenizer, options: DecodeOptions): any | typeof BREAK | typeof DONE {
+function tokenToMap(
+  token: Token,
+  tokeniser: DecodeTokenizer,
+  options: DecodeOptions,
+  path: string[],
+  ignoreToken: boolean = false
+): Record<any, unknown> | Map<string, unknown> {
   const useMaps = options.useMaps === true
-  const obj = useMaps ? undefined : {}
-  const m = useMaps ? new Map() : undefined
+  const obj: Record<string, unknown> = {}
+  const m = useMaps ? new Map<string, unknown>() : undefined
   for (let i = 0; i < token.value; i++) {
-    const key = tokensToObject(tokeniser, options)
+    const key = tokensToObject(tokeniser, options, path, true)
     if (key === BREAK) {
       if (token.value === Infinity) {
         // normal end to indefinite length map
         break
       }
-      throw new Error(`${decodeErrPrefix} got unexpected break to lengthed map`)
+      throw new Error(`${decodeErrPrefix} got unexpected break to finite map`)
     }
     if (key === DONE) {
       throw new Error(`${decodeErrPrefix} found map but not enough entries (got ${i} [no key], expected ${token.value})`)
@@ -114,28 +141,30 @@ function tokenToMap(token: Token, tokeniser: DecodeTokenizer, options: DecodeOpt
         throw new Error(`${decodeErrPrefix} found repeat map key "${key}"`)
       }
     }
-    const value = tokensToObject(tokeniser, options)
+    if (typeof key !== 'string') {
+      throw new Error('key in map is not type string, got: ' + typeof key)
+    }
+    const value = tokensToObject(tokeniser, options, [...path, key], ignoreToken)
     if (value === DONE) {
       throw new Error(`${decodeErrPrefix} found map but not enough entries (got ${i} [no value], expected ${token.value})`)
     }
     if (useMaps) {
-      // @ts-ignore TODO reconsider this .. maybe needs to be strict about key types
+      // @ts-ignore
       m.set(key, value)
     } else {
-      // @ts-ignore TODO reconsider this .. maybe needs to be strict about key types
       obj[key] = value
     }
   }
-  // @ts-ignore c'mon man
+  // @ts-ignore
   return useMaps ? m : obj
 }
 
-/**
- * @param {DecodeTokenizer} tokeniser
- * @param {DecodeOptions} options
- * @returns {any|BREAK|DONE}
- */
-function tokensToObject(tokeniser: DecodeTokenizer, options: DecodeOptions): any | typeof BREAK | typeof DONE {
+function tokensToObject(
+  tokeniser: DecodeTokenizer,
+  options: DecodeOptions,
+  path: string[] = [],
+  ignoreToken: boolean = false
+): unknown | typeof BREAK | typeof DONE {
   // should we support array as an argument?
   // check for tokenIter[Symbol.iterator] and replace tokenIter with what that returns?
   if (tokeniser.done()) {
@@ -149,34 +178,37 @@ function tokensToObject(tokeniser: DecodeTokenizer, options: DecodeOptions): any
   }
 
   if (token.type.terminal) {
+    if (!ignoreToken) {
+      options.visitor?.(path, token.value)
+    }
     return token.value
   }
 
-  if (token.type === Type.array) {
-    return tokenToArray(token, tokeniser, options)
+  if (token.type === Type.array || token.type.major === 4) {
+    return tokenToArray(token, tokeniser, options, path, ignoreToken)
   }
 
-  if (token.type === Type.map) {
-    return tokenToMap(token, tokeniser, options)
+  if (token.type === Type.map || token.type.major === 5) {
+    return tokenToMap(token, tokeniser, options, path, ignoreToken)
   }
 
   if (token.type === Type.tag) {
     if (options.tags && typeof options.tags[token.value] === 'function') {
-      const tagged = tokensToObject(tokeniser, options)
-      return options.tags[token.value](tagged)
+      const tagged = tokensToObject(tokeniser, options, path, true)
+      const res = options.tags[token.value](tagged)
+      if (!ignoreToken) {
+        options.visitor?.(path, res)
+      }
+      return res
     }
     throw new Error(`${decodeErrPrefix} tag not supported (${token.value})`)
   }
+  // console.log(token)
   /* c8 ignore next */
   throw new Error('unsupported')
 }
 
-/**
- * @param {Uint8Array} data
- * @param {DecodeOptions} [options]
- * @returns {[any, Uint8Array]}
- */
-function decodeFirst(data: Uint8Array, options?: DecodeOptions): [any, Uint8Array] {
+function decodeFirst(data: Uint8Array, options: DecodeOptions): [unknown, Uint8Array] {
   if (!(data instanceof Uint8Array)) {
     throw new Error(`${decodeErrPrefix} data to decode must be a Uint8Array`)
   }
@@ -189,6 +221,7 @@ function decodeFirst(data: Uint8Array, options?: DecodeOptions): [any, Uint8Arra
   if (decoded === BREAK) {
     throw new Error(`${decodeErrPrefix} got unexpected break`)
   }
+  // @ts-ignore
   return [decoded, data.subarray(tokeniser.pos())]
 }
 
@@ -197,7 +230,7 @@ function decodeFirst(data: Uint8Array, options?: DecodeOptions): [any, Uint8Arra
  * @param {DecodeOptions} [options]
  * @returns {any}
  */
-function decode(data: Uint8Array, options?: DecodeOptions): any {
+function cborgdecode(data: Uint8Array, options: DecodeOptions): unknown {
   const [decoded, remainder] = decodeFirst(data, options)
   if (remainder.length > 0) {
     throw new Error(`${decodeErrPrefix} too many terminals, data makes no sense`)
@@ -205,4 +238,19 @@ function decode(data: Uint8Array, options?: DecodeOptions): any {
   return decoded
 }
 
-export { Tokeniser, tokensToObject, decode, decodeFirst }
+const _decodeOptions = {
+  allowIndefinite: false,
+  coerceUndefinedToNull: true,
+  allowNaN: false,
+  allowInfinity: false,
+  allowBigInt: true, // this will lead to BigInt for ints outside of
+  // safe-integer range, which may surprise users
+  strict: true,
+  useMaps: false,
+  rejectDuplicateMapKeys: true,
+  tags: []
+}
+
+type ByteView<T> = Uint8Array
+export const decode = <T = unknown>(data: ByteView<T>, visitor?: (path: string[], value: unknown) => void): T =>
+  cborgdecode(data, { ..._decodeOptions, visitor }) as T
